cfd8dcdf7edb302700a00b62dbdea12410041276
Rohit Kandimalla <rohit.kandimalla@semanticbits.com>
2022-11-15 09:39:45 -0500

    Created a new component AutoComplete along with few stories

---
+++ react/components/AutoComplete/AutoComplete.jsx
@@ -0,0 +1,153 @@
+import React from "react";
+import { FormControl, FormHelperText, Autocomplete as MUIAutoComplete, TextField, Checkbox } from "@mui/material";
+import PropTypes from "prop-types";
+import InputLabel from "../InputLabel";
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+
+const autoCompleteStyles = {
+    borderRadius: "3px",
+    height: 40,
+    border: "1px solid #DDDDDD",
+    "& .MuiOutlinedInput-notchedOutline": {
+        borderRadius: "3px",
+        "& legend": {
+            width: 0,
+        },
+    },
+    "& .MuiAutocomplete-inputFocused": {
+        border: "none",
+        boxShadow: "none",
+        outline: "none",
+    },
+    "& .MuiAutocomplete-inputRoot": {
+        paddingTop: 0,
+        paddingBottom: 0,
+    },
+};
+
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+
+// unique props are required when we have duplicate options
+const renderOptionSingle = (props, option) => {
+    const uniqueProps = {
+        ...props,
+        key: `${props.key}_${props.id}`,
+    };
+    return <li {...uniqueProps}>{option}</li>;
+};
+
+const renderOptionMultiple = (props, option, selected) => {
+    const uniqueProps = {
+        ...props,
+        key: `${props.key}_${props.id}`,
+    };
+    return (
+        <li {...uniqueProps}>
+            <Checkbox
+                icon={icon}
+                checkedIcon={checkedIcon}
+                style={{ marginRight: 8 }}
+                checked={selected}
+            />
+            {option}
+        </li>
+    );
+};
+
+const AutoComplete = ({
+    id,
+    dataTestId,
+    options = [],
+    disabled = false,
+    label,
+    placeholder,
+    multiple = false,
+    required = false,
+    error = false,
+    helperText,
+    onChange,
+    ...rest
+}) => {
+    return (
+        <FormControl error={error} fullWidth>
+            <InputLabel
+                htmlFor={`${id}`}
+                id={`${id}-label`}
+                disabled={disabled}
+                required={required}
+                shrink
+            >
+                {label}
+            </InputLabel>
+            <MUIAutoComplete
+                 disablePortal
+                 id={id}
+                 data-testid={dataTestId}
+                 options={options}
+                 disabled={disabled}
+                 multiple={multiple}
+                 disableCloseOnSelect={multiple}
+                 sx={ multiple ? {
+                     ...autoCompleteStyles,
+                     height: "auto",
+                     "& .MuiAutocomplete-inputRoot": {
+                         // This styling are necessary
+                         paddingTop: 1,
+                         paddingBottom: 1,
+                     },
+                 } : autoCompleteStyles}
+                 renderInput={(params) => {
+                     const { inputProps } = params;
+                     inputProps["aria-required"] = required;
+                     inputProps["aria-describedby"] = `${id}-helper-text`;
+                     inputProps["aria-labelledby"] = `${id}-label`;
+                     return (
+                         <TextField
+                             {...params}
+                             placeholder={placeholder}
+                             inputProps={inputProps}
+                             label=""
+                             error={error}
+                         />
+                     );
+                 }}
+                 onChange={(_event, selectedVal) => onChange(id, selectedVal || "")}
+                 renderOption={(props, option, { selected}) => {
+                     debugger;
+                     return multiple ? renderOptionMultiple(props, option, selected)
+                         : renderOptionSingle(props, option)
+                 }}
+                 {...rest}
+             />
+             {helperText && (
+                 <FormHelperText
+                     tabIndex={0}
+                     aria-live="polite"
+                     id={`${id}-helper-text`}
+                     data-testid={`${id}-helper-text`}
+                     error={error}
+                 >
+                     {helperText}
+                 </FormHelperText>
+             )}
+        </FormControl>
+    )
+};
+
+AutoComplete.propTypes = {
+    id: PropTypes.string,
+    dataTestId: PropTypes.string,
+    placeholder: PropTypes.string,
+    required: PropTypes.bool,
+    disabled: PropTypes.bool,
+    multiple: PropTypes.bool,
+    error: PropTypes.bool,
+    label: PropTypes.string,
+    helperText: PropTypes.string,
+    options: PropTypes.arrayOf(PropTypes.element),
+    onChange: PropTypes.any,
+};
+
+export default AutoComplete;
---
+++ react/components/AutoComplete/AutoComplete.stories.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import AutoComplete from "./AutoComplete";
+
+export default {
+    title: "AutoComplete",
+    component: AutoComplete,
+}
+
+const options = ["Option1", "Option2", "Option3", "Option4", "Option5"]
+
+const TemplateSingleSelect = (args) => {
+    const [value, setValue] = useState("");
+    const handleChange = (id, selectedVal) => {
+        setValue(selectedVal);
+    }
+    return (
+        <div className="qpp-u-padding--16" >
+            <AutoComplete
+                id="autocomplete"
+                dataTestId="autocomplete"
+                label= "Auto Complete"
+                options={options}
+                onChange={handleChange}
+                value={value}
+                {...args}
+            />
+        </div>
+    );
+}
+
+export const AutoCompleteWithLabel = TemplateSingleSelect.bind({});
+AutoCompleteWithLabel.args = {
+    placeholder: "Search for a value"
+}
+
+export const AutoCompleteWithLabelDisabled = TemplateSingleSelect.bind({});
+AutoCompleteWithLabelDisabled.args = {
+    disabled: true,
+    placeholder: "Search for a value"
+}
+
+export const AutoCompleteWithHelperText = TemplateSingleSelect.bind({});
+AutoCompleteWithHelperText.args = {
+    helperText: "Helper text goes here",
+    placeholder: "Search for a value"
+}
+
+export const AutoCompleteRequired = TemplateSingleSelect.bind({});
+AutoCompleteRequired.args = {
+    required: true,
+    placeholder: "Search for a value"
+}
+
+export const AutoCompleteWithError = TemplateSingleSelect.bind({});
+AutoCompleteWithError.args = {
+    required: true,
+    helperText: "This field is required",
+    error: true
+}
+
+const TemplateMultipleSelect = (args) => {
+    const [value, setValue] = useState(["Option1", "Option2"]);
+    const handleChange = (id, selectedVal) => {
+        setValue(selectedVal);
+    }
+    return (
+        <div className="qpp-u-padding--16" >
+            <AutoComplete
+                id="autocomplete"
+                dataTestId="autocomplete"
+                label= "Auto Complete"
+                multiple={true}
+                options={options}
+                onChange={handleChange}
+                value={value}
+                {...args}
+            />
+        </div>
+    );
+}
+
+export const AutoCompleteMultipleWithLabel = TemplateMultipleSelect.bind({});
+AutoCompleteMultipleWithLabel.args = {
+    placeholder: "Start typing to search for a value"
+}
+
+export const AutoCompleteMultipleDisabled = TemplateMultipleSelect.bind({});
+AutoCompleteMultipleDisabled.args = {
+    disabled: true,
+    placeholder: "Start typing"
+}
+
+export const AutoCompleteMultipleWithError = TemplateMultipleSelect.bind({});
+AutoCompleteMultipleWithError.args = {
+    required: true,
+    helperText: "At least one field is required",
+    error: true
+}
