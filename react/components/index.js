cd350cb1529f6eb143e7cb87282ff6e34ff964ec
sb-prateekkeerthi <prateek.keerthi@semanticbits.com>
2022-11-15 19:43:23 -0500

    adding modal for deleting

--- react/components/index.js
+++ react/components/index.js
@@ -11,6 +11,7 @@ import FormControlLabel from "./FormControlLabel";
 import MadieAlert from "./MadieAlert";
 import MadieDialog from "./MadieDialog";
 import MadieDiscardDialog from "./MadieDiscardDialog";
+import MadieDeleteDialog from "./MadieDeleteDialog";
 import MadieSpinner from "./MadieSpinner";
 import Modal from "./Modal";
 import { Pagination } from "./Pagination";
@@ -126,6 +127,7 @@ export {
     MadieAlert,
     MadieDialog,
     MadieDiscardDialog,
+    MadieDeleteDialog,
     MadieSpinner,
     Pagination,
     ReadOnlyTextField,
