a7560204aeac1bd21e4b7a84d755f1f486bab89c
sb-prateekkeerthi <prateek.keerthi@semanticbits.com>
2022-12-06 09:18:24 -0500

    added popover

---
+++ react/components/Popover/Popover.stories.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import Popover from "./index";
+import { withKnobs } from "@storybook/addon-knobs";
+
+export default {
+    title: "Popover",
+    component: Popover,
+    decorators: [withKnobs],
+};
+
+export const PopOver = () => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [optionsOpen, setOptionsOpen] = useState(false);
+
+    const onClose = () => {
+        setOptionsOpen(false);
+    };
+
+    const handleOpen = (e) => {
+        setAnchorEl(e.currentTarget);
+        setOptionsOpen(true);
+    };
+
+    const viewEditredirect=()=>{
+        setOptionsOpen(false);
+    }
+
+    const zipData=()=>{
+        setOptionsOpen(false);
+    }
+
+    return (
+        <div className="qpp-u-padding--12">
+            <button className="qpp-c-button" onClick={(e) => handleOpen(e)}>
+                Open Popover
+            </button>
+            <Popover
+                optionsOpen={optionsOpen}
+                anchorEl={anchorEl}
+                onClose={onClose}
+                canEdit={true}
+                editViewSelectOptionProps={{
+                    label:"View",
+                    toImplementFunction: viewEditredirect,
+                    dataTestId: `edit-measure-1`
+                  }}
+                  otherSelectOptionProps={{
+                    label: "Export",
+                    toImplementFunction: zipData,
+                    dataTestId:`export-measure-1`
+                  }}  
+            />
+        </div>
+    );
+};
---
+++ react/components/Popover/index.js
@@ -0,0 +1,100 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Popover from "@mui/material/Popover";
+
+const MadiePopover = ({
+    optionsOpen,
+    anchorEl,
+    handleClose,
+    canEdit,
+    editViewSelectOptionProps,
+    otherSelectOptionProps,
+}) => {
+    return (
+        <Popover
+            open={optionsOpen}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "right",
+            }}
+            transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+            }}
+            sx={{
+                ".MuiPopover-paper": {
+                    boxShadow: "none",
+                    overflow: "visible",
+                    ".popover-content": {
+                        border: "solid 1px #979797",
+                        position: "relative",
+                        marginTop: "16px",
+                        marginLeft: "-70px",
+                        borderRadius: "6px",
+                        background: "#F7F7F7",
+                        width: "115px",
+                        "&::before": {
+                            borderWidth: "thin",
+                            position: "absolute",
+                            top: "-8px",
+                            left: "calc(50% - 8px)",
+                            height: "16px",
+                            width: "16px",
+                            background: "#F7F7F7",
+                            borderColor:
+                                "#979797 transparent transparent #979797",
+                            content: '""',
+                            transform: "rotate(45deg)",
+                        },
+                        ".btn-container": {
+                            display: "flex",
+                            flexDirection: "column",
+                            padding: "10px 0",
+                            button: {
+                                zIndex: 2,
+                                fontSize: 14,
+                                padding: "0px 12px",
+                                textAlign: "left",
+                                "&:hover": {
+                                    backgroundColor: "rgba(0, 0, 0, 0.5)",
+                                },
+                            },
+                        },
+                    },
+                },
+            }}
+        >
+            <div className="popover-content" data-testid="popover-content">
+                <div className="btn-container">
+                    <button
+                        data-testid={editViewSelectOptionProps.dataTestId}
+                        onClick={editViewSelectOptionProps.toImplementFunction}
+                    >
+                        {editViewSelectOptionProps.label}
+                    </button>
+                    {canEdit && (
+                        <button
+                            data-testid={otherSelectOptionProps.dataTestId}
+                            onClick={otherSelectOptionProps.toImplementFunction}
+                        >
+                            {otherSelectOptionProps.label}
+                        </button>
+                    )}
+                </div>
+            </div>
+        </Popover>
+    );
+};
+
+MadiePopover.propTypes = {
+    optionsOpen: PropTypes.bool,
+    anchorEl: PropTypes.any,
+    handleClose: PropTypes.func,
+    canEdit: PropTypes.bool,
+    editViewSelectOptionProps: PropTypes.object,
+    otherSelectOptionProps: PropTypes.object,
+};
+
+export default MadiePopover;
