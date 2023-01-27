ffc2dfbc4d20047bd08daf444ed7f324781a6ced
mcmcphillips <matt.mcphillips@semanticbits.com>
2023-01-27 10:43:31 -0800

    Update accordian test

--- react/test/components/Accordion.test.js
+++ react/test/components/Accordion.test.js
@@ -1,12 +1,22 @@
+import "@testing-library/jest-dom";
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
 import Accordion from "../../components/Accordion";
 
 describe("Accordion", () => {
-    it("Should render okay", () => {
-        const { getByTestId } = render(
-            <Accordion title="Title">Content</Accordion>
+    it("Should render okay", async() => {
+        const { getByTestId, getByRole } = render(
+            <Accordion title="Title" centerItem="centerItem" rightItem="rightItem" leftItem="leftItem">Content</Accordion>
         );
         expect(getByTestId("accordion")).toBeTruthy();
+        const button = getByRole("button")
+        fireEvent.click(button)
+        await waitFor(() => {
+            expect(button).toHaveClass("active")
+        });
+        fireEvent.click(button) 
+        await waitFor(() => {
+            expect(button).not.toHaveClass("active")
+        });
     });
 });
