fbace202558bcae371e327a205ab079ae23eb518
sb-prateekkeerthi <prateek.keerthi@semanticbits.com>
2023-06-06 12:25:51 -0400

    added datetime resusable component

---
+++ react/test/components/DateTimeField.test.js
@@ -0,0 +1,31 @@
+import "@testing-library/jest-dom";
+import { describe, expect, test } from "@jest/globals";
+import DateTimeField from "../../components/DateTimeField/DateTimeField";
+import { act } from "react-dom/test-utils";
+import { render } from "@testing-library/react";
+import dayjs from "dayjs";
+
+import * as React from "react";
+
+describe("DateTimeField", () => {
+    test("DateTimeField Exists", async () => {
+        await act(async () => {
+            const { findByText, findByTestId, getByDisplayValue } = render(
+                <DateTimeField
+                    label="Active"
+                    dateTimeValue={dayjs("2022-04-17T15:30")}
+                    handleDateTimeChange={() => {
+                        return;
+                    }}
+                />
+            );
+
+            const labelText = await findByText("Active Date/Time");
+            expect(labelText).toBeInTheDocument();
+            expect(await findByTestId("active-date/time")).toBeInTheDocument();
+            expect(
+                getByDisplayValue("04/17/2022 03:30 PM")
+            ).toBeInTheDocument();
+        });
+    });
+});
