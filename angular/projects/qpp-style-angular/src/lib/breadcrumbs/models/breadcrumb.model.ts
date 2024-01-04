1b980436911f757c55e47eb134ea313cfde7a0bf
mcmcphillips <matt.mcphillips@icf.com>
2024-01-04 11:17:09 -0800

    MAT-6459: Resolve critical dependencies outside of React

--- angular/projects/qpp-style-angular/src/lib/breadcrumbs/models/breadcrumb.model.ts
+++
@@ -1,6 +0,0 @@
-export interface BreadcrumbItem {
-  url: string;
-  title: string;
-  queryParams?: { [k: string]: any };
-  external?: boolean;
-}
--- angular/projects/qpp-style-angular/src/lib/file-upload/file-upload.model.ts
+++
@@ -1,8 +0,0 @@
-export interface FileErrorEvent {
-  errorTitle: string;
-  errorDescription: string;
-}
-
-export enum ErrorTypeHeader {
-  FileUpload = 'Failed to Upload File',
-}
