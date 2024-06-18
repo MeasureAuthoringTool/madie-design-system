41ffb7250758e9184849db11118e69493f06f960
Rohit Kandimalla <rohit.kandimalla@icf.com>
2024-06-18 11:24:54 -0400

    MAT-7151 Updated NumberInput component with custom configs

--- react/components/NumberInput/NumberInput.stories.js
+++ react/components/NumberInput/NumberInput.stories.js
@@ -1,6 +1,7 @@
 import React from "react";
-import NumberInput from "./index";
 import { withKnobs } from "@storybook/addon-knobs";
+import NumberInput from "./index";
+import PropTypes from "prop-types";
 
 export default {
     title: "NumberInput",
@@ -8,14 +9,80 @@ export default {
     decorators: [withKnobs],
 };
 
-const [value, setValue] = React.useState(null);
-export const BasicNumberInput = () => (
-    <NumberInput
-        aria-label="Demo number input"
-        placeholder="Type a number…"
-        value={value}
-        onChange={(event, val) => setValue(val)}
-    />
+const Wrapper = ({ children }) => (
+    <div className="qpp-u-padding--16" style={{ width: 300 }}>
+        {children}
+    </div>
 );
+Wrapper.propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.node,
+};
 
-// NumberInput.storyName = "Basic NumberInput";
+export const BasicNumberInput = () => {
+    return (
+        <Wrapper>
+            <NumberInput
+                id="basic-number-input"
+                label="Demo number input"
+            />
+        </Wrapper>
+    )
+};
+
+export const NumberInputWithNoNegativeNumbers = () => {
+    return (
+        <Wrapper>
+            <NumberInput
+                id="basic-number-input"
+                label="Demo number input"
+                aria-label="Demo number input"
+                placeholder="Type a number…"
+                allowNegative={false}
+            />
+        </Wrapper>
+    )
+};
+
+export const NumberInputWithErrorAndHelperText = () => {
+    return (
+        <Wrapper>
+            <NumberInput
+                id="basic-number-input"
+                error={true}
+                label="Demo number input"
+                aria-label="Demo number input"
+                placeholder="Type a number…"
+                helperText={"Please Enter a valid integer"}
+            />
+        </Wrapper>
+    )
+};
+
+export const NumberInputDisabled = () => {
+    return (
+        <Wrapper>
+            <NumberInput
+                id="basic-number-input"
+                label="Demo number input"
+                aria-label="Demo number input"
+                placeholder="Type a number…"
+                disabled={true}
+            />
+        </Wrapper>
+    )
+};
+
+export const NumberInputWithTooltip = () => {
+    return (
+        <Wrapper>
+            <NumberInput
+                id="basic-number-input"
+                label="Demo number input"
+                aria-label="Demo number input"
+                placeholder="Type a number…"
+                tooltipText={"Provides information"}
+            />
+        </Wrapper>
+    )
+};
--- react/components/NumberInput/index.jsx
+++ react/components/NumberInput/index.jsx
@@ -1,165 +1,70 @@
 import * as React from 'react';
-import {
-    Unstable_NumberInput as BaseNumberInput,
-    numberInputClasses,
-} from '@mui/base/Unstable_NumberInput';
-import { styled } from '@mui/system';
+import PropTypes from "prop-types";
+import TextField from "../TextField";
 
-export default function NumberInput(props) {
+export default function NumberInput({
+    id,
+    error = false,
+    helperText = undefined,
+    required = false,
+    disabled = false,
+    label,
+    tooltipText,
+    inputProps,
+    allowNegative = true,
+    ...rest
+}) {
+    const newInputProps = { ...inputProps } || {};
+    if (!newInputProps["data-testid"]) {
+        newInputProps["data-testid"] = `${id}-input`;
+    }
+    // if aria-describedBy is not provided, add it depending on helper-text and tooltip presence
+    if (!newInputProps["aria-describedby"]) {
+        let newDescribedBy = "";
+        if (helperText) {
+            newDescribedBy += `${id}-helper-text `;
+        }
+        if (tooltipText) {
+            newDescribedBy += `${id}-tooltip`;
+        }
+        if (newDescribedBy) {
+            newInputProps["aria-describedby"] = newDescribedBy;
+        }
+    }
+    newInputProps["type"] = "number";
+    if (!allowNegative) {
+        newInputProps["min"] = 0;
+    }
     return (
-        <BaseNumberInput
-            slots={{
-                root: StyledInputRoot,
-                input: StyledInputElement,
-                incrementButton: StyledButton,
-                decrementButton: StyledButton,
+        <TextField
+            id={`integer-field-${id}`}
+            error={error}
+            helperText={helperText}
+            required={required}
+            disabled={disabled}
+            label={label}
+            tooltipText={tooltipText}
+            inputProps={newInputProps}
+            placeholder={`Enter ${label}`}
+            data-testid={`integer-field-${id}`}
+            onKeyDown={(e) => {
+                if (e.key === "e" || e.key === "E" || e.key === "+" || e.key === "-") {
+                    e.preventDefault();
+                }
             }}
-            slotProps={{
-                incrementButton: {
-                    children: '▴',
-                },
-                decrementButton: {
-                    children: '▾',
-                },
-            }}
-            {...props}
+            {...rest}
         />
     );
 }
 
-const blue = {
-    100: '#DAECFF',
-    200: '#80BFFF',
-    400: '#3399FF',
-    500: '#007FFF',
-    600: '#0072E5',
+NumberInput.propTypes = {
+    id: PropTypes.string,
+    error: PropTypes.bool,
+    helperText: PropTypes.string,
+    required: PropTypes.bool,
+    disabled: PropTypes.bool,
+    label: PropTypes.string,
+    tooltipText: PropTypes.string,
+    inputProps: PropTypes.object,
+    allowNegative: PropTypes.bool,
 };
-
-const grey = {
-    50: '#F3F6F9',
-    100: '#E5EAF2',
-    200: '#DAE2ED',
-    300: '#C7D0DD',
-    400: '#B0B8C4',
-    500: '#9DA8B7',
-    600: '#6B7A90',
-    700: '#434D5B',
-    800: '#303740',
-    900: '#1C2025',
-};
-
-const StyledInputRoot = styled('div')(
-    ({ theme }) => `
-  font-family: 'IBM Plex Sans', sans-serif;
-  font-weight: 400;
-  border-radius: 8px;
-  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
-  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
-  border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
-  box-shadow: 0px 2px 2px ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
-  display: grid;
-  grid-template-columns: 1fr 19px;
-  grid-template-rows: 1fr 1fr;
-  overflow: hidden;
-  column-gap: 8px;
-  padding: 4px;
-
-  &.${numberInputClasses.focused} {
-    border-color: ${blue[400]};
-    box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? blue[600] : blue[200]};
-  }
-
-  &:hover {
-    border-color: ${blue[400]};
-  }
-
-  // firefox
-  &:focus-visible {
-    outline: 0;
-  }
-`,
-);
-
-const StyledInputElement = styled('input')(
-    ({ theme }) => `
-  font-size: 0.875rem;
-  font-family: inherit;
-  font-weight: 400;
-  line-height: 1.5;
-  grid-column: 1/2;
-  grid-row: 1/3;
-  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
-  background: inherit;
-  border: none;
-  border-radius: inherit;
-  padding: 8px 12px;
-  outline: 0;
-`,
-);
-
-const StyledButton = styled('button')(
-    ({ theme }) => `
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: center;
-  align-items: center;
-  appearance: none;
-  padding: 0;
-  width: 19px;
-  height: 19px;
-  font-family: system-ui, sans-serif;
-  font-size: 0.875rem;
-  line-height: 1;
-  box-sizing: border-box;
-  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
-  border: 0;
-  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
-  transition-property: all;
-  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-  transition-duration: 120ms;
-
-  &:hover {
-    background: ${theme.palette.mode === 'dark' ? grey[800] : grey[50]};
-    border-color: ${theme.palette.mode === 'dark' ? grey[600] : grey[300]};
-    cursor: pointer;
-  }
-
-  &.${numberInputClasses.incrementButton} {
-    grid-column: 2/3;
-    grid-row: 1/2;
-    border-top-left-radius: 4px;
-    border-top-right-radius: 4px;
-    border: 1px solid;
-    border-bottom: 0;
-    &:hover {
-      cursor: pointer;
-      background: ${blue[400]};
-      color: ${grey[50]};
-    }
-
-  border-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[200]};
-  background: ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
-  color: ${theme.palette.mode === 'dark' ? grey[200] : grey[900]};
-  }
-
-  &.${numberInputClasses.decrementButton} {
-    grid-column: 2/3;
-    grid-row: 2/3;
-    border-bottom-left-radius: 4px;
-    border-bottom-right-radius: 4px;
-    border: 1px solid;
-    &:hover {
-      cursor: pointer;
-      background: ${blue[400]};
-      color: ${grey[50]};
-    }
-
-  border-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[200]};
-  background: ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
-  color: ${theme.palette.mode === 'dark' ? grey[200] : grey[900]};
-  }
-  & .arrow {
-    transform: translateY(-1px);
-  }
-`,
-);
