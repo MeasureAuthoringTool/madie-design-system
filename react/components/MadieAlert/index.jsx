45140397b0562199b4574cf78a06bf2b2523af42
Ethan Kaplan <ethan.kaplan@semanticbits.com>
2024-08-13 13:29:46 -0700

    MAT-6188 add option to copy madie alert text with button

--- react/components/MadieAlert/index.jsx
+++ react/components/MadieAlert/index.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 import ErrorIcon from "@mui/icons-material/Error"; // warning
 import InfoIcon from "@mui/icons-material/Info"; // info
 import CancelIcon from "@mui/icons-material/Cancel"; // error
 import CheckCircleIcon from "@mui/icons-material/CheckCircle"; //success
+import ContentCopyIcon from '@mui/icons-material/ContentCopy'; //copy
 
 import { IconButton } from "@mui/material";
 import ClearIcon from "@mui/icons-material/Clear";
@@ -19,7 +20,43 @@ const MadieAlert = ({
     canClose = true,
     alertProps, // props to pass to outer component
     closeButtonProps, // props to pass to close button
+    copyButton
+
 }) => {
+    const copyButtonBuilder = (content) => {
+        const traversal = (contentNode, parentNode = true) => {
+            if (!contentNode) return '';
+            
+            let result = '';
+    
+            if (Array.isArray(contentNode.props?.children)) {
+                contentNode.props.children.forEach((child, index) => {
+                    result += traversal(child, false);
+                    if (parentNode && index < contentNode.props.children.length - 1) {
+                        result += '\n';
+                    }else if(contentNode.type ==="ul" ){
+                        result = '\n'+result+'\n'
+                    }
+                });
+            } else if (typeof contentNode.props?.children === 'string' || typeof contentNode.props?.children === 'number') {
+                result += contentNode.props.children.toString();
+            } else if (typeof contentNode === 'string' || typeof contentNode === 'number') {
+                result += contentNode.toString();
+            } else if (React.isValidElement(contentNode)) {
+                result += traversal(contentNode, false);
+            }
+    
+            return result;
+        };
+    
+        return traversal(content);
+    };
+
+    useEffect(() => {
+        if(content && copyButton){
+    copyText=copyButtonBuilder(content);
+    console.log(copyText)}
+    }, [content]);
     // we have four states to render for
     const typeSelect = {
         warning: ErrorIcon,
@@ -27,6 +64,7 @@ const MadieAlert = ({
         error: CancelIcon,
         success: CheckCircleIcon,
     };
+    let copyText = ""
     const Icon = typeSelect[type];
 
     const alertClass = classNames("madie-alert", type);
@@ -37,6 +75,29 @@ const MadieAlert = ({
             <div className={alertClass} {...alertProps}>
                 <Icon className={iconClass} />
                 <div id="content">{content && content}</div>
+                {copyButton && (
+                    <IconButton
+                    sx={{
+                        marginLeft: "auto",
+                        "&:after": {
+                            content: `''`,
+                            position: "absolute",
+                            left: "0px",
+                            width: "1px",
+                            height: "40px",
+                            backgroundColor: "#B0B0B0",
+                            pointerEvents: "none",
+                        },
+                    }}
+                    
+                >
+                    <ContentCopyIcon onClick={(e)=>{
+                        e.preventDefault()
+                        navigator.clipboard.writeText(copyText);
+                        
+                    }} sx={{ color: "#242424" }} />
+                </IconButton>
+                )}
                 {canClose && (
                     <IconButton
                         sx={{
@@ -68,6 +129,10 @@ MadieAlert.propTypes = {
     canClose: PropTypes.bool,
     alertProps: PropTypes.object,
     closeButtonProps: PropTypes.object,
+    copyButton:PropTypes.bool,
+};
+MadieAlert.defaultProps = {
+    copyButton:false
 };
 
 export default MadieAlert;
