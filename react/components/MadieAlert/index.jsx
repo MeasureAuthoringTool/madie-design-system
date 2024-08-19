1f5036232879ad5d739b0e38ba951f39215ee826
Ethan Kaplan <ethan.kaplan@semanticbits.com>
2024-08-19 07:09:26 -0700

    MAT-6188 check format

--- react/components/MadieAlert/index.jsx
+++ react/components/MadieAlert/index.jsx
@@ -1,10 +1,10 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 import ErrorIcon from "@mui/icons-material/Error"; // warning
 import InfoIcon from "@mui/icons-material/Info"; // info
 import CancelIcon from "@mui/icons-material/Cancel"; // error
 import CheckCircleIcon from "@mui/icons-material/CheckCircle"; //success
-import ContentCopyIcon from '@mui/icons-material/ContentCopy'; //copy
+import ContentCopyIcon from "@mui/icons-material/ContentCopy"; //copy
 
 import { IconButton } from "@mui/material";
 import ClearIcon from "@mui/icons-material/Clear";
@@ -20,40 +20,45 @@ const MadieAlert = ({
     canClose = true,
     alertProps, // props to pass to outer component
     closeButtonProps, // props to pass to close button
-    copyButton
-
+    copyButton,
 }) => {
     const [copyText, setCopyText] = useState("");
 
     const copyButtonBuilder = (content) => {
         const traversal = (contentNode, parentNode = true) => {
-            if (!contentNode) return '';
-            let result = '';
+            if (!contentNode) return "";
+            let result = "";
 
-            if (Array.isArray(contentNode.props?.children )) {
+            if (Array.isArray(contentNode.props?.children)) {
                 contentNode.props.children.forEach((child, index) => {
                     result += traversal(child, false);
-                    if (parentNode && index ==1) {
-                        result += '\n';
-                    }else if(contentNode.type ==="ul" ){
-                        result = '\n'+result+'\n'
+                    if (parentNode && index == 1) {
+                        result += "\n";
+                    } else if (contentNode.type === "ul") {
+                        result = "\n" + result + "\n";
                     }
                 });
-            } else if(typeof contentNode.props?.children ==='object'){
+            } else if (typeof contentNode.props?.children === "object") {
                 result += traversal(contentNode.props.children, false);
-            } else if (typeof contentNode.props?.children === 'string' || typeof contentNode.props?.children === 'number') {
+            } else if (
+                typeof contentNode.props?.children === "string" ||
+                typeof contentNode.props?.children === "number"
+            ) {
                 result += contentNode.props.children.toString();
-            } else if (typeof contentNode === 'string' || typeof contentNode === 'number') {
+            } else if (
+                typeof contentNode === "string" ||
+                typeof contentNode === "number"
+            ) {
                 result += contentNode.toString();
-            } 
+            }
             return result;
         };
         return traversal(content).trim();
     };
 
     useEffect(() => {
-        if(content && copyButton){
-            setCopyText(copyButtonBuilder(content))
+        if (content && copyButton) {
+            setCopyText(copyButtonBuilder(content));
         }
     }, [content]);
     // we have four states to render for
@@ -74,25 +79,27 @@ const MadieAlert = ({
                 <div id="content">{content && content}</div>
                 {copyButton && (
                     <IconButton
-                    sx={{
-                        marginLeft: "auto",
-                        "&:after": {
-                            content: `''`,
-                            position: "absolute",
-                            left: "0px",
-                            width: "1px",
-                            height: "40px",
-                            backgroundColor: "#B0B0B0",
-                            pointerEvents: "none",
-                        },
-                    }}
-                    
-                >
-                    <ContentCopyIcon onClick={(e)=>{
-                        e.preventDefault()
-                        navigator.clipboard.writeText(copyText);
-                    }} sx={{ color: "#242424" }} />
-                </IconButton>
+                        sx={{
+                            marginLeft: "auto",
+                            "&:after": {
+                                content: `''`,
+                                position: "absolute",
+                                left: "0px",
+                                width: "1px",
+                                height: "40px",
+                                backgroundColor: "#B0B0B0",
+                                pointerEvents: "none",
+                            },
+                        }}
+                    >
+                        <ContentCopyIcon
+                            onClick={(e) => {
+                                e.preventDefault();
+                                navigator.clipboard.writeText(copyText);
+                            }}
+                            sx={{ color: "#242424" }}
+                        />
+                    </IconButton>
                 )}
                 {canClose && (
                     <IconButton
@@ -125,10 +132,10 @@ MadieAlert.propTypes = {
     canClose: PropTypes.bool,
     alertProps: PropTypes.object,
     closeButtonProps: PropTypes.object,
-    copyButton:PropTypes.bool,
+    copyButton: PropTypes.bool,
 };
 MadieAlert.defaultProps = {
-    copyButton:false
+    copyButton: false,
 };
 
 export default MadieAlert;
