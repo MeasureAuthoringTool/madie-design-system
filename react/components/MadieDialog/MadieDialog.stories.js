39e9603f188b2c8b5419ca884b0ee6b34068cc44
sb-cecilialiu <cecilia.liu@icf.com>
2025-04-29 07:24:00 -0500

    MAT-8383 show tooltip for disabled continue button

--- react/components/MadieDialog/MadieDialog.stories.js
+++ react/components/MadieDialog/MadieDialog.stories.js
@@ -6,7 +6,7 @@ import TextArea from "../TextArea/index";
 import Select from "../Select/index";
 import Button from "../Button";
 import { Switch, FormGroup, FormControlLabel } from "@mui/material";
-import { DialogContent, Typography } from "@mui/material";
+import { DialogContent, Typography, Tooltip } from "@mui/material";
 
 export default {
     title: "MadieDialog",
@@ -216,6 +216,59 @@ export const DialogWithActionButtonsHideContinueButton = () => {
                     "data-testid": "cancel-button",
                 }}
                 continueButtonProps={""}
+            >
+                <DialogContent>
+                    <div data-testid="view-modal">
+                        <Typography>
+                            <div>Some Text</div>
+                        </Typography>
+                    </div>
+                </DialogContent>
+            </MadieDialog>
+        </>
+    );
+};
+
+export const DialogWithActionButtonsContinueButtonDisabledWithTooltip = () => {
+    const [open, setOpen] = useState(false);
+    const onClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <>
+            <Button variant="cyan" onClick={() => setOpen(true)}>
+                open Dialog
+            </Button>
+            <MadieDialog
+                title="Human Readable"
+                sx={{
+                    "#modal-body": {
+                        h2: {
+                            borderTop: "1px solid black",
+                        },
+                    },
+                }}
+                dialogProps={{
+                    onClose,
+                    open,
+                    maxWidth: "lg",
+                    fullWidth: true,
+                }}
+                cancelButtonProps={{
+                    variant: "secondary",
+                    cancelText: "Cancel",
+                    "data-testid": "cancel-button",
+                }}
+                continueButtonProps={{
+                    variant: "cyan",
+                    type: "submit",
+                    "data-testid": "continue-button",
+                    continueText: "Continue",
+                    disabled: true,
+                    tooltipText: "Continue button is disabled",
+
+                }}
             >
                 <DialogContent>
                     <div data-testid="view-modal">
